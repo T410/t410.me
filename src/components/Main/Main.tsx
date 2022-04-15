186c7b4eee3f6c7f8be550652893da42c16ce105
T410 <tymnt410@gmail.com>
2022-04-15 16:03:14 +0300

    Fix a reponsivity issue

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -5,7 +5,7 @@ const Main = () => {
 	return (
 		<div className="h-full flex sm:grid sm:grid-cols-layout gap-2 bg-primary-900">
 			<Sidebar />
-			<div className="w-full h-full p-5 rounded bg-primary-700">
+			<div className="w-full min-h-full h-fit p-5 rounded bg-primary-700">
 				<Outlet />
 			</div>
 		</div>

ba268082ddca0e66e09e852c9b395fe340ff3449
T410 <tymnt410@gmail.com>
2022-04-15 16:03:15 +0300

    Change Projects style

--- src/routes/Projects/Projects.tsx
+++ src/routes/Projects/Projects.tsx
@@ -39,37 +39,44 @@ const Projects = () => {
 	}, []);
 
 	return (
-		<div className="h-full bg-navy-700 card">
+		<>
 			{loading ? (
 				<div className="h-full w-full flex flex-row justify-center items-center">
 					<img className="animate-spin w-20 h-20" src={spinner} alt="Loading Spinner"></img>
 				</div>
 			) : (
-				<div className="grid grid-cols-1 auto-rows-fr gap-8 sm:grid-cols-2 xl:grid-cols-3">
+				<div className="grid grid-cols-1 auto-rows-fr gap-2 sm:grid-cols-2">
 					{projects.map((project) => (
 						<div
 							key={project._id}
-							className="card bg-navy-600 text-white min-h-fit h-full flex flex-col justify-between space-y-4 transition-all"
+							className="card bg-neutral-900 text-white min-h-fit h-full flex flex-col justify-between space-y-4 transition-all"
 						>
-							<h2 className="text-2xl flex-1">{project.title}</h2>
-							<p className="text-sm">{project.description}</p>
-							<div className="border-b-4 rounded border-navy-100" />
-							<div className="flex flex-row justify-between">
-								<a href={project.demo} target="_blank" rel="noreferrer">
-									<img src="https://img.shields.io/badge/-Demo-rgb(34%20197%2094)?style=for-the-badge" alt="Demo" />
+							<h1 className="flex-1 font-bold text-2xl md:text-3xl">{project.title}</h1>
+							<p className="text-lg md:text-xl">{project.description}</p>
+							<div className="border-b-2 rounded border-neutral-800" />
+							<div className="flex flex-row center w-full">
+								<a
+									href={project.demo}
+									target="_blank"
+									rel="noreferrer"
+									className="text-indigo-600 text-center border-indigo-600 border-2 rounded-l-lg px-5 py-2 font-bold text-lg w-1/2 mr-[1px] after:content-['_↗'] hover:bg-indigo-600 hover:text-white hover:underline"
+								>
+									Demo
 								</a>
-								<a href={project.source} target="_blank" rel="noreferrer">
-									<img
-										src="https://img.shields.io/badge/-Source-rgb(249%2C115%2C22)?style=for-the-badge"
-										alt="Source"
-									/>
+								<a
+									href={project.source}
+									target="_blank"
+									rel="noreferrer"
+									className="text-teal-600 text-center border-teal-600 border-2 rounded-r-lg px-5 py-2 font-bold text-lg w-1/2 ml-[1px] after:content-['_↗'] hover:bg-teal-600 hover:text-white hover:underline"
+								>
+									Code
 								</a>
 							</div>
 						</div>
 					))}
 				</div>
 			)}
-		</div>
+		</>
 	);
 };
 

6ba12fd05a12484858495c9c6cf66d866353e886
T410 <tymnt410@gmail.com>
2022-04-15 16:03:13 +0300

    More styling

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,6 @@
 import devto from "./devto.svg";
 import github from "./github.svg";
 import linkedin from "./linkedin.svg";
+import spinner from "./spinner.svg";
 
-export { devto, github, linkedin };
+export { devto, github, linkedin, spinner };
--- src/routes/Projects/Projects.tsx
+++ src/routes/Projects/Projects.tsx
@@ -2,6 +2,8 @@ import styles from "./Projects.module.css";
 import { useEffect, useState } from "react";
 import { Project as IProject } from "../../types";
 import { Card } from "../../components";
+import { spinner } from "../../assets";
+
 async function fetchProjects() {
 	return await fetch("https://api.t410.me/.netlify/functions/graphql", {
 		method: "POST",
@@ -30,35 +32,46 @@ async function fetchProjects() {
 
 const Projects = () => {
 	const [projects, setProjects] = useState<IProject[]>([]);
+	const [loading, setLoading] = useState(true);
 	useEffect(() => {
 		fetchProjects().then((projects) => {
 			setProjects(projects);
+			setLoading(false);
 		});
 	}, []);
+
 	return (
-		<div className={styles.outerContainer}>
+		<div>
 			<h1>Projects</h1>
-			<div className={styles.projectsContainer}>
-				{projects.map((project) => (
-					<div key={project._id} className={styles.project}>
-						<Card className={styles.projectCard}>
-							<>
-								<h2>{project.title}</h2>
-								<p className={styles.desc}>{project.description}</p>
-								<div className={styles.horizontalLine} />
-								<div className={styles.links}>
-									<a href={project.demo} target="_blank" rel="noreferrer">
-										Demo Link
-									</a>
-									<a href={project.source} target="_blank" rel="noreferrer">
-										Source
-									</a>
-								</div>
-							</>
-						</Card>
-					</div>
-				))}
-			</div>
+			{loading ? (
+				<div className={styles.loadingContainer}>
+					<img src={spinner} alt="Loading Spinner"></img>
+				</div>
+			) : (
+				<div className={styles.projectsContainer}>
+					{projects.map((project) => (
+						<div key={project._id} className={styles.project}>
+							<Card className={styles.projectCard}>
+								<>
+									<div className={styles.titleContainer}>
+										<h2>{project.title}</h2>
+									</div>
+									<p className={styles.desc}>{project.description}</p>
+									<div className={styles.horizontalLine} />
+									<div className={styles.links}>
+										<a href={project.demo} target="_blank" rel="noreferrer">
+											Demo Link
+										</a>
+										<a href={project.source} target="_blank" rel="noreferrer">
+											Source
+										</a>
+									</div>
+								</>
+							</Card>
+						</div>
+					))}
+				</div>
+			)}
 		</div>
 	);
 };

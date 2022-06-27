3b91e714d65bcec7f83be49a21fc23f712349a43
T410 <tymnt410@gmail.com>
2022-06-27 15:35:37 +0300

    Add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import { AppContextProvider } from "contexts";
 import { AppProps } from "next/app";
-import "../styles/globals.css";
 import { Navbar } from "components";
+import { AnalyticsInit } from "elements/Analytics";
+
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<AppContextProvider>
+			<AnalyticsInit />
 			<Navbar />
 
 			<main className="max-w-3xl m-auto flex mt-14 flex-col p-4 md:p-8">
